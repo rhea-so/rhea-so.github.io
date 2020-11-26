b0f2a4656bdb7f0748d75d5ade4da2436e49f6da
rhea-so <jeonghyeon.rhea@gmail.com>
2020-11-26 23:22:08 +0900

    bug: circle ci test

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull origin main
 git remote set-url --push origin https://rhea-so:$AccessToken@github.com/rhea-so/rhea-so.github.io.git/
 
 git add -fA
-git commit --allow-empty -m "$(git log source -1 --pretty=%B)"
+git commit --allow-empty -m "$(git log post -1 --pretty=%B)"
 git push -f origin main
 
 echo "deployed successfully"
