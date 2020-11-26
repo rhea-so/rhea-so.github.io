02c4310bad0a4b1a2a27cce087e6bac02374fc81
rhea-so <jeonghyeon.rhea@gmail.com>
2020-11-26 23:06:59 +0900

    bug: circle ci test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ git config user.email "$USER_EMAIL"
 git checkout main
 git pull origin main
 
+git remote set-url --push origin https://rhea-so:$AccessToken@github.com/github.com/rhea-so.github.io.git/
+
 git add .
 git commit -m "circle ci"
 git push -f origin main
-git remote -v
-echo $usd
+
 echo "deployed successfully"
