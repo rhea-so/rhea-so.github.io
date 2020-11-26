2c54c21adc33a57fb4c99ea3b72472a43c35935b
rhea-so <jeonghyeon.rhea@gmail.com>
2020-11-26 23:41:56 +0900

    bug: circle ci build fix

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ git pull origin main
 
 git remote set-url --push origin https://rhea-so:$AccessToken@github.com/rhea-so/rhea-so.github.io.git/
 
+rm -rf vendor
+
 git add -fA
 git commit --allow-empty -m "$(git log post -1 --pretty=%B)"
 git push -f origin main
