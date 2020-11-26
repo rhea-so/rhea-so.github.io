d74c9665ae8f79a167f724a6fc635e10ba927746
rhea-so <jeonghyeon.rhea@gmail.com>
2020-11-26 22:55:34 +0900

    bug: circle ci test

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,7 @@ git config user.email "$USER_EMAIL"
 git checkout main
 git pull origin main
 
-find . -maxdepth 1 ! -name '_site' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
-mv _site/* .
-rm -R _site/
-
-git add -fA
+git add .
 git commit -m "circle ci"
 git push -f origin main
 
