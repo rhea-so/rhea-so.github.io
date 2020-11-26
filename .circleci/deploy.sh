e8acbf7d43387ca62605efb723b484272af0c567
rhea-so <jeonghyeon.rhea@gmail.com>
2020-11-26 22:44:41 +0900

    bug: circle ci tet

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,12 +4,12 @@ git config user.email "$USER_EMAIL"
 git checkout main
 git pull origin main
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name '_site' ! -name '.gitignore' ! -name '.circleci' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
 git add -fA
-git commit --allow-empty -m "$(git log source -1 --pretty=%B)"
+git commit -m "circle ci"
 git push -f origin main
 
 echo "deployed successfully"
