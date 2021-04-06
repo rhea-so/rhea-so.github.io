3462c6afac89594044bda93c1b81c44afbb8e8f7
rhea-so <jeonghyeon.rhea@gmail.com>
2021-04-06 23:15:28 +0900

    fix: Gemfile

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-rm -rf vendor
+git checkout main
+git pull origin main
 
 git remote set-url --push origin https://rhea-so:$AccessToken@github.com/rhea-so/rhea-so.github.io.git/
 
+rm -rf vendor
+
 git add -fA
 git commit --allow-empty -m "$(git log post -1 --pretty=%B)"
-
-git push origin main --force
-
+git push --force origin main
 
 echo "deployed successfully"
