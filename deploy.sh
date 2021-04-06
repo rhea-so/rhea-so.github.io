bd14ded40e150bf8fc5a479ee097e5f0008ec1db
rhea-so <jeonghyeon.rhea@gmail.com>
2021-04-06 23:02:18 +0900

    fix: deploy.sj

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
-git checkout main
-git pull origin main
+rm -rf vendor
 
 git remote set-url --push origin https://rhea-so:$AccessToken@github.com/rhea-so/rhea-so.github.io.git/
 
-rm -rf vendor
-
 git add -fA
 git commit --allow-empty -m "$(git log post -1 --pretty=%B)"
+
+git checkout main
+git pull origin main
 git push -f origin main
 
 echo "deployed successfully"
